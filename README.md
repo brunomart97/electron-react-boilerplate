370786affa23b48d23a3047e317a13450555e58a
Bruno Martins <brunomart97@hotmail.com>
2024-03-09 16:44:51 -0300

    feat: electron with react boilerplate

---
+++ README.md
@@ -0,0 +1,53 @@
+# Getting Started
+
+### Dependencies
+
+Install the dependencies
+
+```shell
+npm install
+```
+
+### Development environment
+
+Running the project
+
+In a terminal, run react:
+
+```shell
+npm run react
+```
+
+In another terminal, run electron:
+
+```shell
+npm run electron
+```
+
+### Build the project
+
+First, run the react build:
+
+```shell
+npm run build-react
+```
+
+Then, run the electron build for the chosen operating system:
+
+Windows:
+
+```shell
+npm run build-electron-win
+```
+
+Mac:
+
+```shell
+npm run build-electron-mac
+```
+
+Linux:
+
+```shell
+npm run build-electron-linux
+```
---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+const isDev = true
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 500,
+    height: 500,
+    resizable: false,
+    maximizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    autoHideMenuBar: true,
+  })
+
+  const startUrl = isDev ? 'http://localhost:3000' : url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  window.loadURL(startUrl)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
